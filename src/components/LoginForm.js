e4c3459e355684ac0bcd34db97e9a9e0c9aeb297
Adam O'Connell <AdamMac@Adams-MBP.local>
2017-10-29 17:30:48 -0400

    wire up auth reducers

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 
@@ -8,6 +8,11 @@ class LoginForm extends Component {
   onEmailChange(text) {
     this.props.emailChanged(text);
   }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
   render() {
     return (
       <Card>
@@ -16,6 +21,7 @@ class LoginForm extends Component {
             label="Email"
             placeholder="user123@gmail.com"
             onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
           />
         </CardSection>
 
@@ -24,6 +30,8 @@ class LoginForm extends Component {
           secureTextEntry
           label="Password"
           placeholder="password"
+          onChangeText={this.onPasswordChange.bind(this)}
+          value={this.props.password}
         />
         </CardSection>
 
@@ -37,4 +45,11 @@ class LoginForm extends Component {
   }
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email,
+    password: state.auth.password
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
