9a58cf4eaf8fa090c5d7ea16762b5ab67423fe42
AOKOdesign <adam@aoko.ca>
2017-11-01 20:25:01 -0400

    add create employee page

--- src/Router.js
+++ src/Router.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import { Stack, Scene, Router } from 'react-native-router-flux';
+import { Stack, Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
       <Stack key="root">
-<<<<<<< HEAD
         <Scene key="auth">
           <Scene key="login" component={LoginForm} title="Please Login" />
         </Scene>
 
         <Scene key="main">
           <Scene
-          onRight={() => console.log('right!!!')}
+          onRight={() => Actions.employeeCreate()}
           rightTitle="Add"
           key="employeeList"
           component={EmployeeList}
-          title="Employees" 
+          title="Employees"
+          initial
           />
+          <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
         </Scene>
-=======
-        <Scene key="login" component={LoginForm} title="Please Login" />
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
->>>>>>> e3e41126c588bd970f61d834c791f5b4d70c4c26
       </Stack>
     </Router>
   );
