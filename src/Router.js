f4a8f508ba41302b01bbd05f4f2461eb35bfcde0
AOKOdesign <adam@aoko.ca>
2017-11-05 16:58:08 -0500

    wire up employee editing

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Stack, Scene, Router, Actions } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Stack key="root">
         <Scene key="auth">
           <Scene key="login" component={LoginForm} title="Please Login" />
         </Scene>
@@ -22,8 +22,8 @@ const RouterComponent = () => {
           initial
           />
           <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+          <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
         </Scene>
-      </Stack>
     </Router>
   );
 };
