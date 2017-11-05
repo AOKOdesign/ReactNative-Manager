44399a04ad67630bb1706d4ae8089023575ff379
AOKOdesign <adam@aoko.ca>
2017-11-05 17:42:06 -0500

    style Confirm.js modal

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -4,16 +4,19 @@ import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, cardSectionStyle, textStyle } = styles;
   return (
     <Modal
       visible={visible}
-      transparent="true"
+      transparent
       animationType="slide"
       onRequestClose={() => {}}
     >
-      <View>
-        <CardSection>
-          <Text>{children}</Text>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>
+            {children}
+          </Text>
         </CardSection>
 
         <CardSection>
@@ -23,6 +26,24 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
       </View>
     </Modal>
   );
+};
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
 };
 
 export { Confirm };
